0365a5e9d9da38a1cc257ebe61855c99aa915874
klunker_roox <klunker.roox@gmail.com>
2025-09-09 16:52:31 +0200

    add: added try catch to API controller

--- src/Http/Controllers/SubscriberController.php
+++ src/Http/Controllers/SubscriberController.php
@@ -2,88 +2,126 @@
 
 namespace Klunker\LaravelSubscribe\Http\Controllers;
 
+use Throwable;
 use Illuminate\Routing\Controller;
-use Klunker\LaravelSubscribe\Events\SubscriberCreated;
-use Klunker\LaravelSubscribe\Events\SubscriberDeleted;
-use Klunker\LaravelSubscribe\Events\SubscriberUpdated;
 use Klunker\LaravelSubscribe\Facades\Subscribe;
 use Klunker\LaravelSubscribe\Http\Requests\DeleteRequest;
 use Klunker\LaravelSubscribe\Http\Requests\StoreSubscriberRequest;
 use Klunker\LaravelSubscribe\Http\Requests\UnsubscribeRequest;
 use Klunker\LaravelSubscribe\Http\Requests\ViewSubscriberRequest;
-use Klunker\LaravelSubscribe\Model\Subscriber;
 
 class SubscriberController extends Controller
 {
     public function view(ViewSubscriberRequest $request)
     {
-        $subscriber = Subscribe::getSubscriber($request->validated('email'));
-        return response()->json([
-            'subscriber' => $subscriber
-        ]);
+        try {
+            $subscriber = Subscribe::getSubscriber($request->validated('email'));
+            return response()->json([
+                'subscriber' => $subscriber
+            ]);
+        } catch (Throwable $e) {
+            return response()->json([
+                'message' => 'Error while processing request',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function store(StoreSubscriberRequest $request)
     {
-        $subscriber = Subscribe::subscribe(
-            $request->validated('email'),
-            $request->validated('name'),
-            $request->validated('subscribe_on')
-        );
+        try {
+            $subscriber = Subscribe::subscribe(
+                $request->validated('email'),
+                $request->validated('name'),
+                $request->validated('subscribe_on')
+            );
 
-        $wasRecentlyCreated = $subscriber->wasRecentlyCreated;
+            $wasRecentlyCreated = $subscriber->wasRecentlyCreated;
 
-        return response()->json([
-            'message' => $wasRecentlyCreated ? 'Subscriber created' : 'Subscriber updated',
-            'subscriber' => $subscriber->refresh()
-        ], $wasRecentlyCreated ? 201 : 200);
+            return response()->json([
+                'message' => $wasRecentlyCreated ? 'Subscriber created' : 'Subscriber updated',
+                'subscriber' => $subscriber->refresh()
+            ], $wasRecentlyCreated ? 201 : 200);
+        } catch (Throwable $e) {
+            return response()->json([
+                'message' => 'Error while processing request',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function unsubscribe(UnsubscribeRequest $request)
     {
-        $subscriber = Subscribe::getSubscriberByToken($request->validated('token'));
-        $subscriber = Subscribe::updateSubscriber(
-            $subscriber,
-            $request->validated('name'),
-            $request->validated('subscribe_on')
-        );
+        try {
+            $subscriber = Subscribe::getSubscriberByToken($request->validated('token'));
+            $subscriber = Subscribe::updateSubscriber(
+                $subscriber,
+                $request->validated('name'),
+                $request->validated('subscribe_on')
+            );
 
-        return response()->json([
-            'message' => 'Subscriber updated',
-            'subscriber' => $subscriber->refresh()
-        ]);
+            return response()->json([
+                'message' => 'Subscriber updated',
+                'subscriber' => $subscriber->refresh()
+            ]);
+        } catch (Throwable $e) {
+            return response()->json([
+                'message' => 'Error while processing request',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function delete(DeleteRequest $request)
     {
-        $subscriber = Subscribe::getSubscriberByToken($request->validated('token'));
-        if (!$subscriber) {
+        try {
+            $subscriber = Subscribe::getSubscriberByToken($request->validated('token'));
+            if (!$subscriber) {
+                return response()->json([
+                    'message' => 'Subscriber not found',
+                ], 404);
+            }
+            $isDeleted = Subscribe::deleteSubscriber($subscriber);
             return response()->json([
-                'message' => 'Subscriber not found',
-            ], 404);
+                'message' => $isDeleted
+                    ? 'Subscriber deleted'
+                    : 'Subscriber not deleted',
+            ]);
+        } catch (Throwable $e) {
+            return response()->json([
+                'message' => 'Error while processing request',
+                'error' => $e->getMessage()
+            ], 500);
         }
-        $isDeleted = Subscribe::deleteSubscriber($subscriber);
-        return response()->json([
-            'message' => $isDeleted
-                ? 'Subscriber deleted'
-                : 'Subscriber not deleted',
-        ]);
-
     }
 
     public function token(ViewSubscriberRequest $request)
     {
-        $token = Subscribe::getToken($request->validated('email'));
-        return response()->json([
-            'token' => $token
-        ]);
+        try {
+            $token = Subscribe::getToken($request->validated('email'));
+            return response()->json([
+                'token' => $token
+            ]);
+        } catch (Throwable $e) {
+            return response()->json([
+                'message' => 'Error while processing request',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     public function channels()
     {
-        return response()->json([
-            'channels' => Subscribe::getChannels()
-        ]);
+        try {
+            return response()->json([
+                'channels' => Subscribe::getChannels()
+            ]);
+        } catch (Throwable $e) {
+            return response()->json([
+                'message' => 'Error while processing request',
+                'error' => $e->getMessage()
+            ], 500);
+        }
 
     }
 
